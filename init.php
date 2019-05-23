e9e1a3baa967a24c5faac20918ec9b3119f8d5f8
Joemari713 <50824233+Joemari713@users.noreply.github.com>
2019-05-23 16:31:25 +0800

    Update init.php
    Added DB_PORT

--- init.php
+++ init.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/lib/const.php';
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/lib/func.php';
 
-$db = new \Models\DB(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD);
+$db = new \Models\DB(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT);
 \Models\Model::set_db($db);
 
 // register tables
